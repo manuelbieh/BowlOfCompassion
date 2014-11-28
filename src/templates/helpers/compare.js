e77dfe786b9323901cce7577fb05b4146527ac9d
Manuel Bieh <github@manuelbieh.de>
2014-11-28 21:57:52 +0100

    Content content content

--- src/templates/helpers/compare.js
+++ src/templates/helpers/compare.js
@@ -35,4 +35,40 @@ module.exports.register = function (Handlebars, options)  {
 
     });
 
+
+    // Handlebars.registerHelper('compare', function(lvalue, operator, rvalue) {
+
+    //     if (arguments.length < 3) {
+    //         throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+    //     }
+
+    //     op = operator;
+
+    //     var operators = {
+    //         '==':       function(l,r) { return l == r; },
+    //         '===':      function(l,r) { return l === r; },
+    //         '!=':       function(l,r) { return l != r; },
+    //         '<':        function(l,r) { return l < r; },
+    //         '>':        function(l,r) { return l > r; },
+    //         '<=':       function(l,r) { return l <= r; },
+    //         '>=':       function(l,r) { return l >= r; },
+    //         '%':        function(l,r) { return l%r == 0; },
+    //         '%1':        function(l,r) { return l%r == 1; },
+    //         'typeof':   function(l,r) { return typeof l == r; }
+    //     };
+
+    //     if (!operators[op]) {
+    //         throw new Error("Handlerbars Helper 'compare' doesn't know the operator " + op);
+    //     }
+
+    //     var result = operators[op](lvalue, rvalue);
+
+    //     if( result ) {
+    //         return options.fn(this);
+    //     } else {
+    //         return options.inverse(this);
+    //     }
+
+    // });
+
 };
--- src/templates/helpers/pagemeta.js
+++ src/templates/helpers/pagemeta.js
@@ -7,25 +7,35 @@ module.exports.register = function (Handlebars, options)  {
 		this.page = meta;
 
 		if(this.menu && this.menu.items) {
+
 			this.menu.items.forEach(function(item) {
+
 				// console.log(meta.active, item.href, meta.active.indexOf(item.href) > -1);
-				if(meta.active && meta.active.indexOf(item.href) > -1) {
+				// console.log(meta.active, item.id, meta.active && meta.active.indexOf(item.id));
+				if(meta.active && (meta.active.indexOf(item.href) > -1 || meta.active.indexOf(item.id) > -1)) {
 					item.active = true;
 				}
 
 				if(item.items) {
+
 					item.items.forEach(function(subitem) {
-						if(meta.active && meta.active.indexOf(subitem.href) > -1) {
+						if(meta.active && (meta.active.indexOf(subitem.href) > -1 || meta.active.indexOf(subitem.id) > -1)) {
 							subitem.active = true;
 						}
 					});
+
 				}
 
 			});
+
 		}
 
 		if(typeof meta.category !== 'undefined') {
 			this.category = meta.category;
+		}
+
+		if(typeof meta.title !== 'undefined') {
+			this.title = meta.title;
 		}
 
 		if(typeof meta.filename !== 'undefined') {
