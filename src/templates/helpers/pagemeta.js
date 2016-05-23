4e67f511c87aba55ee3f190d9eda600895064809
Manuel <github@manuelbieh.de>
2016-05-23 14:35:41 +0200

    converted tabs to spaces

--- src/templates/helpers/pagemeta.js
+++ src/templates/helpers/pagemeta.js
@@ -2,56 +2,56 @@ module.exports.register = function (Handlebars, options)  {
 
   Handlebars.registerHelper('pagemeta', function (meta)  {
 
-	if(meta) {
+    if(meta) {
 
-		// this.page = meta;
-		this.pagemeta = meta;
+        // this.page = meta;
+        this.pagemeta = meta;
 
-		if(this.menu && this.menu.items) {
+        if(this.menu && this.menu.items) {
 
-			this.menu.items.forEach(function(item) {
+            this.menu.items.forEach(function(item) {
 
-				// console.log(meta.active, item.href, meta.active.indexOf(item.href) > -1);
-				// console.log(meta.active, item.id, meta.active && meta.active.indexOf(item.id));
-				if(meta.active && (meta.active.indexOf(item.href) > -1 || meta.active.indexOf(item.id) > -1)) {
-					item.active = true;
-				}
+                // console.log(meta.active, item.href, meta.active.indexOf(item.href) > -1);
+                // console.log(meta.active, item.id, meta.active && meta.active.indexOf(item.id));
+                if(meta.active && (meta.active.indexOf(item.href) > -1 || meta.active.indexOf(item.id) > -1)) {
+                    item.active = true;
+                }
 
-				if(item.items) {
+                if(item.items) {
 
-					item.items.forEach(function(subitem) {
-						if(meta.active && (meta.active.indexOf(subitem.href) > -1 || meta.active.indexOf(subitem.id) > -1)) {
-							subitem.active = true;
-						}
-					});
+                    item.items.forEach(function(subitem) {
+                        if(meta.active && (meta.active.indexOf(subitem.href) > -1 || meta.active.indexOf(subitem.id) > -1)) {
+                            subitem.active = true;
+                        }
+                    });
 
-				}
+                }
 
-			});
+            });
 
-		}
+        }
 
-		if(typeof meta.category !== 'undefined') {
-			this.category = meta.category;
-		}
+        if(typeof meta.category !== 'undefined') {
+            this.category = meta.category;
+        }
 
-		if(typeof meta.title !== 'undefined') {
-			this.title = meta.title;
-		}
+        if(typeof meta.title !== 'undefined') {
+            this.title = meta.title;
+        }
 
         if(typeof meta.description !== 'undefined') {
-			this.description = meta.description;
-		}
+            this.description = meta.description;
+        }
 
         if(typeof meta.keywords !== 'undefined') {
-			this.keywords = meta.keywords;
-		}
+            this.keywords = meta.keywords;
+        }
 
-		if(typeof meta.filename !== 'undefined') {
-			this.pagination.index.dest = this.pagination.index.filePair.orig.dest + '/' + meta.filename
-		}
+        if(typeof meta.filename !== 'undefined') {
+            this.pagination.index.dest = this.pagination.index.filePair.orig.dest + '/' + meta.filename
+        }
 
-	}
+    }
 
   });
 
